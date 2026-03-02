04faef535118fb713b3ef769497b3632aa18ca92
mohammedalith <mohammedalith713@gmail.com>
2026-03-02 11:19:32 +0530

    'login'

--- docs/auth.js
+++ docs/auth.js
@@ -2,7 +2,7 @@
 (function () {
     const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:3000'
-        : 'https://documentation-34fr.onrender.com';
+        : 'https://document-docsify.onrender.com';
 
     const AUTH_KEYS = {
         TOKEN: 'gh_access_token',
